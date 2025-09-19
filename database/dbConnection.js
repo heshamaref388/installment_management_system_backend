3f8c9113ab1c8ee48c81ecf87b4fa8fdf947fd74
heshamaref388 <heshamaref388@gmail.com>
2025-09-20 00:37:03 +0300

    first commit

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,20 +1,18 @@
 import mongoose from "mongoose";
 
-export const  dbConnection = async() => {
-   mongoose
-    .connect("mongodb+srv://heshamaref3888:Hesham2020@cluster0.slc6oyv.mongodb.net/installment_management_system", {
+export const dbConnection = async () => {
+  try {
+    await mongoose.connect("mongodb+srv://heshamaref3888:Hesham2020@cluster0.slc6oyv.mongodb.net/installment_management_system", {
       serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
       socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
       maxPoolSize: 10, // Maintain up to 10 socket connections
-      bufferCommands: false, // Disable mongoose buffering
-      bufferMaxEntries: 0 // Disable mongoose buffering
-    })
-    .then(() => {
-      console.log("Connected to MongoDB successfully");
-    })
-    .catch((err) => {
-      console.log("Failed to connect to MongoDB", err);
+      bufferCommands: false // Disable mongoose buffering
     });
+    console.log("Connected to MongoDB successfully");
+  } catch (err) {
+    console.log("Failed to connect to MongoDB", err);
+    throw err; // Re-throw to prevent server from starting if connection fails
+  }
 };
 
 
