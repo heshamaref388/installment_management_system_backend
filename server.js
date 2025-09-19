e8692315797549cefcf03cc01fb021cb80de8658
heshamaref388 <heshamaref388@gmail.com>
2025-09-20 00:51:31 +0300

    first commit

--- server.js
+++ server.js
@@ -18,11 +18,14 @@ const app = express();
 const port = 3000;
 await dbConnection();
 app.use(cors({
-  origin: 'https://installment-management-system-front.vercel.app',
-  credentials: true
-},{
-  origin: 'http://localhost:5173',
-  credentials: true
+  origin: [
+    'https://installment-management-system-front.vercel.app',
+    'http://localhost:5173',
+    'http://localhost:3000'
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 app.use(express.static("uploads"));
