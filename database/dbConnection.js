bc897ad04e1ead39a801d1229c451a8b3e8b2bf1
heshamaref388 <heshamaref388@gmail.com>
2025-09-20 00:21:39 +0300

    first commit

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -3,11 +3,11 @@ import mongoose from "mongoose";
 export const dbConnection = () => {
   mongoose
     .connect("mongodb+srv://heshamaref3888:Hesham2020@cluster0.slc6oyv.mongodb.net/installment_management_system", {
-      // serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-      // socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-      // maxPoolSize: 10, // Maintain up to 10 socket connections
-      // bufferCommands: false, // Disable mongoose buffering
-      // bufferMaxEntries: 0 // Disable mongoose buffering
+      serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+      socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+      maxPoolSize: 10, // Maintain up to 10 socket connections
+      bufferCommands: false, // Disable mongoose buffering
+      bufferMaxEntries: 0 // Disable mongoose buffering
     })
     .then(() => {
       console.log("Connected to MongoDB successfully");
